093fc93827af1a04c4bb377df5ce7611329fdd3b
Joanthan Vinicius <141459667+Jonathanfullstack@users.noreply.github.com>
2024-02-08 19:41:27 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,122 @@
+// Efeito principal
+window.revelar = ScrollReveal({reset:false}); // reset false para acontecer somente uma vez na pagina, caso queria q aconteça td hr entao coloque como "true"
+
+// TODO DO SITE EFEITO
+revelar.reveal('.efeito-txt-top', {
+    //duraçao  //2s
+    duration: 2000,
+    distance: '90px' //distancia para o deslocamento - baixo para cima
+});
+
+revelar.reveal('.efeito-img-topo', {
+    //duraçao  //2s
+    //distancia para o deslocamento - baixo para cima
+    duration: 2000,
+    distance: '90px', 
+    delay: 500,
+});
+
+// Efeito sobre
+revelar.reveal('.efeito-img-sobre', {
+    //duraçao  //2s
+    //distancia para o deslocamento - baixo para cima
+    duration: 1500,
+    distance: '90px', 
+    delay: 500,
+    origin: 'left',
+});
+
+revelar.reveal('.efeito-titulo-sobre', {
+    //duraçao  //2s
+    //distancia para o deslocamento - baixo para cima
+    duration: 1500,
+    distance: '90px', 
+    delay: 500,
+    origin: 'left',
+});
+
+
+revelar.reveal('.efeito-bio-sobre', {
+    //duraçao  //2s
+    //distancia para o deslocamento - baixo para cima
+    duration: 1500,
+    distance: '90px', 
+    delay: 500,
+    origin: 'left',
+});
+
+
+
+
+//Efeito dos Projetos
+
+revelar.reveal('.efeito-projetos1', {
+    //duraçao  //2s
+    //distancia para o deslocamento - baixo para cima
+    duration: 2000,
+    distance: '90px', 
+    delay: 500,
+});
+
+revelar.reveal('.efeito-projetos2', {
+    //duraçao  //2s
+    //distancia para o deslocamento - baixo para cima
+    duration: 2000,
+    distance: '90px', 
+    delay: 1000
+
+});
+
+revelar.reveal('.efeito-projetos3', {
+    //duraçao  //2s
+    //distancia para o deslocamento - baixo para cima
+    duration: 2000,
+    distance: '90px', 
+    delay: 1500,
+ 
+});
+
+
+revelar.reveal('.efeito-btn', {
+    //duraçao  //2s
+    //distancia para o deslocamento - baixo para cima
+    duration: 2000,
+    distance: '90px', 
+    delay: 1800,
+
+});
+
+
+// Efeito rolagem 
+
+revelar.reveal('.rolagem-projeto1', {
+    //duraçao  //2s
+    //distancia para o deslocamento - baixo para cima
+    duration: 2000,
+    distance: '90px', 
+    delay: 500
+});
+
+revelar.reveal('.rolagem-projeto2', {
+    //duraçao  //2s
+    //distancia para o deslocamento - baixo para cima
+    duration: 2000,
+    distance: '90px', 
+    delay: 500
+});
+
+revelar.reveal('.rolagem-projeto3 ', {
+    //duraçao  //2s
+    //distancia para o deslocamento - baixo para cima
+    duration: 2000,
+    distance: '90px', 
+    delay: 500
+});
+
+revelar.reveal('.rolagem-projeto4', {
+    //duraçao  //2s
+    //distancia para o deslocamento - baixo para cima
+    duration: 2000,
+    distance: '90px', 
+    delay: 500
+});
